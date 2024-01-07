608565d5ec8b4c3bcc8fe60cace9f031d94cdb98
larry-xue <azoux0311@gmail.com>
2024-01-07 20:48:04 +0800

    feat: 允许跨域

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
