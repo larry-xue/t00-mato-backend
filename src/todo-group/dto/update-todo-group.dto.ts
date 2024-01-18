691b98b9df8342c68267769230d45b1adfb5c84b
larry-xue <azoux0311@gmail.com>
2024-01-18 13:48:40 +0800

    fix: remove todos params in update todo-group

--- src/todo-group/dto/update-todo-group.dto.ts
+++ src/todo-group/dto/update-todo-group.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateTodoGroupDto } from './create-todo-group.dto';
-import { IsArray, IsString } from 'class-validator';
 
-export class UpdateTodoGroupDto extends PartialType(CreateTodoGroupDto) {
-  @IsString({ each: true })
-  @IsArray()
-  todos: string[];
-}
+export class UpdateTodoGroupDto extends PartialType(CreateTodoGroupDto) {}
