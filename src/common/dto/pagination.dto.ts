e9888c3378ab1b8a34318b917ba28bd634169584
larry-xue <azoux0311@gmail.com>
2024-01-19 09:30:57 +0800

    fix: 修复pagination校验问题

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, Min, Max } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNumber, Min } from 'class-validator';
 
 export class PaginationDto {
   @IsNumber()
   @ApiProperty()
   @Min(1)
+  @Type(() => Number)
   readonly page: number = 1;
 
   @IsNumber()
   @ApiProperty()
   @Min(1)
+  @Type(() => Number)
   readonly page_size: number = 10;
 }
